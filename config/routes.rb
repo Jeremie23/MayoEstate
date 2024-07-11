844f4ee1a3aa31c83fdcf5be85220dfee2d0640b
Yassir Maghzaz <127986901+YassirMaghzaz@users.noreply.github.com>
2024-07-11 21:20:51 +0100

    add a showing page and link between existing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#home", as: :home
   get "offers", to: "pages#offers"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
